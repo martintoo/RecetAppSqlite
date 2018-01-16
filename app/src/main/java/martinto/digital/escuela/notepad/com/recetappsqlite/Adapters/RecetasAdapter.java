412e02bbbf95e40a8b0ea5e2aa30bc576999232a
Ernesto Martin Torres Garcia <tinto.volk22@gmail.com>
2018-01-16 16:06:23 -0500

    Implementamos ViewHolder y Cardview

--- app/src/main/java/martinto/digital/escuela/notepad/com/recetappsqlite/Adapters/RecetasAdapter.java
+++ app/src/main/java/martinto/digital/escuela/notepad/com/recetappsqlite/Adapters/RecetasAdapter.java
@@ -1,17 +1,32 @@
 package martinto.digital.escuela.notepad.com.recetappsqlite.Adapters;
 
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import martinto.digital.escuela.notepad.com.recetappsqlite.R;
 
 /**
  * Created by martinto on 16/01/18.
  */
 
 public class RecetasAdapter {
+
     public class ViewHolder extends RecyclerView.ViewHolder{
+        CardView cardView;
+        ImageView imageView;
+        TextView nombre,personas,descripcion,preparacion;
 
         public ViewHolder(View itemView) {
             super(itemView);
+            cardView=(CardView)itemView.findViewById(R.id.cardview);
+            imageView=(ImageView)itemView.findViewById(R.id.imagee);
+            nombre=(TextView)itemView.findViewById(R.id.nombre);
+            personas=(TextView)itemView.findViewById(R.id.personas);
+            descripcion=(TextView)itemView.findViewById(R.id.descripcion);
+            preparacion=(TextView)itemView.findViewById(R.id.preparacion);
         }
     }
 }
