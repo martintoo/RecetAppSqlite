99615d32f0152bf825bde59b3d185f3700eb6ce4
Martinto <tinto.volk22@gmail.com>
2018-01-17 16:29:10 -0500

    Creamos BD

--- app/src/main/java/martinto/digital/escuela/notepad/com/recetappsqlite/Helpers/DBHelper.java
+++ app/src/main/java/martinto/digital/escuela/notepad/com/recetappsqlite/Helpers/DBHelper.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import martinto.digital.escuela.notepad.com.recetappsqlite.SQLConstants;
+
 /**
  * Created by martinto on 16/01/18.
  */
@@ -16,16 +18,18 @@ public class DBHelper extends SQLiteOpenHelper {
     public DBHelper(Context context){
         //CursorFactory null
         //Version->
-        super(context,SQLConstants.DB,null,DB_VERSION);
+        super(context, SQLConstants.DB,null,DB_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL(SQLConstants.SQL_CREATE_TABLE_RECETAS);
     }
+
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        db.execSQL(SQLConstants.SQL_DELETE);
+        onCreate(db);
     }
 }
---
+++ app/src/main/java/martinto/digital/escuela/notepad/com/recetappsqlite/SQLConstants.java
@@ -0,0 +1,34 @@
+package martinto.digital.escuela.notepad.com.recetappsqlite;
+
+/**
+ * Created by martinto on 16/01/18.
+ */
+
+public class SQLConstants {
+    //DB
+    public static final String DB="bdrecetas.db";
+    //TABLES
+    public static final String TABLE_RECETAS="recetas";
+    //COLUMNS
+    public static final String COLUMN_ID="id";
+    public static final String COLUMN_NOMBRE="nombre";
+    public static final String COLUMN_PERSONAS="personas";
+    public static final String COLUMN_DESCRIPCION="descripcion";
+    public static final String COLUMN_PREPARACION="preparacion";
+    public static final String COLUMN_IMAGEN="imagen";
+    public static final String COLUMN_FAV="fav";
+    //QUERYS
+    public static final String SQL_CREATE_TABLE_RECETAS=
+            "CREATE TABLE " + TABLE_RECETAS + "(" +
+                    COLUMN_ID + " TEXT PRIMARY KEY," +
+                    COLUMN_NOMBRE + " TEXT,"+
+                    COLUMN_PERSONAS + " INT,"+
+                    COLUMN_DESCRIPCION + " TEXT,"+
+                    COLUMN_PREPARACION + " TEXT,"+
+                    COLUMN_IMAGEN + " TEXT,"+
+                    COLUMN_FAV+ " INT"+");";
+
+    public static final String SQL_DELETE=
+            "DROP TABLE "+ TABLE_RECETAS;
+
+}
